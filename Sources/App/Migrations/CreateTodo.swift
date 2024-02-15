e4a452b86226aa578b0fc63183bd335789101739
ivan <ivan@example.com>
2024-02-15 09:57:47 +0100

    POST CONSEGUIDO

--- Sources/App/Migrations/CreateTodo.swift
+++ Sources/App/Migrations/CreateTodo.swift
@@ -1,14 +1,19 @@
 import Fluent
 
-struct CreateTodo: AsyncMigration {
+struct CreateVehicles: AsyncMigration {
     func prepare(on database: Database) async throws {
-        try await database.schema("todos")
+        try await database.schema("vehiculos")
             .id()
-            .field("title", .string, .required)
+            .field("marca", .string, .required)
+            .field("modelo", .string, .required)
+            .field("num_ruedas", .int, .required)
+            .field("tipo_combustible", .string, .required)
+            .field("pantalla_central", .bool, .required)
+            .field("tamaño_pantalla", .string, .required)
             .create()
     }
 
     func revert(on database: Database) async throws {
-        try await database.schema("todos").delete()
+        try await database.schema("vehiculos").delete()
     }
 }
